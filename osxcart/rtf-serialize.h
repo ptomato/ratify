a4d949f616c30ca3bd96dfafd92727622ea0c709
Philip Chimento <philip.chimento@gmail.com>
2009-08-19 04:14:45 +0000

    Added LGPL and license notices to the top of each source file.
    git-svn-id: https://osxcart.svn.sourceforge.net/svnroot/osxcart/trunk@87 b2c7ee7d-4edc-4b10-941b-b19b4c2b722c

--- osxcart/rtf-serialize.h
+++ osxcart/rtf-serialize.h
@@ -1,6 +1,24 @@
 #ifndef __OSXCART_RTF_SERIALIZE_H__
 #define __OSXCART_RTF_SERIALIZE_H__
 
+/* Copyright 2009 P. F. Chimento
+This file is part of Osxcart.
+
+Osxcart is free software: you can redistribute it and/or modify it under the
+terms of the GNU Lesser General Public License as published by the Free Software 
+Foundation, either version 3 of the License, or (at your option) any later 
+version.
+
+Osxcart is distributed in the hope that it will be useful, but WITHOUT ANY 
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License along 
+with Osxcart.  If not, see <http://www.gnu.org/licenses/>. */
+
+#include <glib.h>
+#include <gtk/gtk.h>
+
 G_GNUC_INTERNAL guint8 *rtf_serialize(GtkTextBuffer *register_buffer, GtkTextBuffer *content_buffer, const GtkTextIter *start, const GtkTextIter *end, gsize *length);
 
 #endif /* __OSXCART_RTF_SERIALIZE_H__ */
