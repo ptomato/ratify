859d6d7ef937ce755246be393f8a08e371bbc258
Philip Chimento <philip.chimento@gmail.com>
2011-05-16 23:05:59 +0000

    Removed last traces of old GLib version workarounds
    git-svn-id: https://osxcart.svn.sourceforge.net/svnroot/osxcart/trunk@113 b2c7ee7d-4edc-4b10-941b-b19b4c2b722c

--- osxcart/plist-parser.c
+++ osxcart/plist-parser.c
@@ -18,7 +18,7 @@ with Osxcart.  If not, see <http://www.gnu.org/licenses/>. */
 #include <config.h>
 #include <glib/gi18n-lib.h>
 #include <osxcart/plist.h>
-#include "plist-common.h"
+#include "init.h"
 
 /* plist-parser.c - This is a plist parser implemented in GLib's GMarkup SAX
 parser API. It requires Glib 2.18 or later. */
@@ -55,6 +55,70 @@ tags */
 static GMarkupParser plist_parser = { 
 	plist_start, plist_end, NULL, NULL, NULL
 };
+
+
+/* Custom string equality function, for typing convenience */
+static inline gboolean 
+str_eq(const gchar *s1, const gchar *s2)
+{
+	return (g_ascii_strcasecmp(s1, s2) == 0);
+}
+
+/* Check the root element of the plist. Make sure it is named <plist>, and that
+it is version 1.0 */
+static void
+check_plist_element(const gchar *element_name, const gchar **attribute_names, const gchar **attribute_values, GError **error)
+{
+    const gchar *version_string;
+
+	if(!str_eq(element_name, "plist")) {
+		g_set_error(error, G_MARKUP_ERROR, G_MARKUP_ERROR_UNKNOWN_ELEMENT, _("<plist> root element not found; got <%s> instead"), element_name);
+		return;
+	}
+	
+	if(!g_markup_collect_attributes(element_name, attribute_names, attribute_values, error,
+	   G_MARKUP_COLLECT_STRING, "version", &version_string,
+	   G_MARKUP_COLLECT_INVALID))
+	    return;
+	/* Don't free version_string */
+	
+	if(!str_eq(version_string, "1.0"))
+		g_set_error(error, PLIST_ERROR, PLIST_ERROR_BAD_VERSION, _("Unsupported plist version '%s'"), version_string);
+}
+
+/* Assign a value to an already-allocated PlistObject, from an XML element
+<name> with content 'text' */
+static void
+fill_object(PlistObject *current, const gchar *name, const gchar *text, GError **error)
+{
+	/* There should be no text in <true> or <false> */
+	if(str_eq(name, "true") || str_eq(name, "false"))
+		g_set_error(error, G_MARKUP_ERROR, G_MARKUP_ERROR_INVALID_CONTENT, _("<%s> should have no content, but found '%s'"), name, text);
+
+	/* <real> - this assumes that property lists do NOT contain localized 
+	representation of numbers */
+	else if(str_eq(name, "real"))
+		current->real.val = g_ascii_strtod(text, NULL);
+	
+	else if(str_eq(name, "integer"))
+		current->integer.val = atoi(text);
+	
+	else if(str_eq(name, "string"))
+		current->string.val = g_strdup(text);
+
+	else if(str_eq(name, "date")) {
+		if(!g_time_val_from_iso8601(text, &(current->date.val)))
+			g_set_error(error, PLIST_ERROR, PLIST_ERROR_BAD_DATE, _("Could not parse date '%s'"), text);
+	}
+	
+	else if(str_eq(name, "data"))
+		current->data.val = g_base64_decode(text, &(current->data.length));
+
+	/* else - just ignore text in <array> and <dict>, because it could be 
+	whitespace */	
+
+	return;
+}
 
 /* Allocate a new PlistObject depending on the <name> of the XML element being
 processed. We do not know what the content of the element is, so it is not
@@ -227,13 +291,55 @@ plist_end(GMarkupParseContext *context, const gchar *element_name, gpointer user
 	*data = parse_data->current;
 }
 
-/* Real implementation of plist_read_from_string(); move this code into that
-function when plist-parser-oldglib.c is removed */
+/**
+ * plist_read:
+ * @filename: The path to a file containing a property list in XML format.
+ * @error: Return location for an error, or %NULL.
+ *
+ * Reads a property list in XML format from @filename and returns a #PlistObject 
+ * representing the property list.
+ *
+ * Returns: the property list, or %NULL if an error occurred, in which case
+ * @error is set. The property list must be freed with plist_object_free() after
+ * use.
+ */
 PlistObject *
-plist_read_from_string_real(const gchar *string, GError **error)
+plist_read(const gchar *filename, GError **error)
+{
+	gchar *contents;
+	PlistObject *retval;
+
+	osxcart_init();
+	
+	g_return_val_if_fail(filename != NULL, NULL);
+	g_return_val_if_fail(error == NULL || *error == NULL, NULL);
+
+	if(!g_file_get_contents(filename, &contents, NULL, error))
+		return NULL;
+	retval = plist_read_from_string(contents, error);
+	g_free(contents);
+	return retval;
+}
+
+/**
+ * plist_read_from_string:
+ * @string: A string containing a property list in XML format.
+ * @error: Return location for an error, or %NULL.
+ *
+ * Reads a property list in XML format from @string and returns a #PlistObject
+ * representing the property list.
+ *
+ * Returns: the property list, or %NULL if an error occurred, in which case
+ * @error is set. The property list must be freed with plist_object_free() after
+ * use.
+ */
+PlistObject *
+plist_read_from_string(const gchar *string, GError **error)
 {
 	GMarkupParseContext *context;
 	PlistObject *plist = NULL;
+	
+	osxcart_init();
 
 	g_return_val_if_fail(string != NULL, NULL);
 	g_return_val_if_fail(error == NULL || *error == NULL, NULL);
